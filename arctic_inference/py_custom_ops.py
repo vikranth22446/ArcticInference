844d66ccc8d170c796b6d29a211b3b98fbef5e9b
Michael Wyatt <michael.wyatt@snowflake.com>
2025-06-02 17:23:15 -0700

    Fix editable install failure (#87)

--- arctic_inference/py_custom_ops.py
+++ arctic_inference/py_custom_ops.py
@@ -8,10 +8,18 @@ logger = logging.getLogger(__name__)
 
 def try_load_torch_library() -> bool:
     package_name = 'arctic_inference'
-    library_name = 'libCustomOps'
+    module_basename = 'custom_ops'
 
     package_path = __import__(package_name).__path__[0]
-    library_path = os.path.join(package_path, f'{library_name}.so')
+
+    # Dynamically locate the compiled extension (handles .cpython-310... suffix)
+    for file in os.listdir(package_path):
+        if file.startswith(module_basename) and file.endswith('.so'):
+            library_path = os.path.join(package_path, file)
+            break
+    else:
+        logger.info("Could not find compiled custom_ops library in package.")
+        return False
 
     try:
         logger.info(f"Attempting to load custom ops from {library_path}...")
