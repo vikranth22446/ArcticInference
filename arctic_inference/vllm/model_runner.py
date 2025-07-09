6a4a6176ba4f56225f3a922add38cdecd63e51ba
Ye Wang <ye.wang@snowflake.com>
2025-07-09 07:20:26 -0700

    Add json_mode benchmark (#121)

--- arctic_inference/vllm/model_runner.py
+++ arctic_inference/vllm/model_runner.py
@@ -671,11 +671,13 @@ class GPUModelRunnerPatch(ArcticPatch[GPUModelRunner]):
             num_sampled_ids = len(sampled_ids)
             
             if (num_sampled_ids == 0):
+                if self.speculative_config.enable_suffix_decoding:
+                    return [[]] * len(sampled_token_ids)
                 req_id = self.input_batch.req_ids[i]
                 req_state = self.requests[req_id]
                 seq_len = (req_state.num_computed_tokens +
                            scheduler_output.num_scheduled_tokens[req_id])
-                sampled_ids = req_state.get_token_id(seq_len)
+                sampled_ids = [req_state.get_token_id(seq_len)]
 
             # Add sampled_token_ids to token_ids_cpu.
             start_idx = self.input_batch.num_tokens_no_spec[i]
