8b6acbf60ea85018b0ea701963f699072664ff25
Vikranth Srivatsa <vsrivatsa@ucsd.edu>
2025-09-09 13:04:48 -0700

    clean artic inference proposer

--- arctic_inference/vllm/spec_dec/arctic_proposer.py
+++ arctic_inference/vllm/spec_dec/arctic_proposer.py
@@ -122,14 +122,6 @@ class ArcticProposer:
                 f"hidden_states shape mismatch: {sample_hidden_states.shape[-1]} != {self.input_hidden_dim}. \
                 Please make sure spec model is trained using the same base model."
         
-        # if isinstance(sampled_token_ids, list):
-        #     # Pad the list of lists to create a uniform tensor
-        #     max_len = max(len(x) for x in sampled_token_ids) if sampled_token_ids else 0
-        #     if max_len == 0:
-        #         return sample_hidden_states
-        #     padded_ids = [l + [-1] * (max_len - len(l)) for l in sampled_token_ids]
-        #     sampled_token_ids = torch.tensor(padded_ids,
-        #                                  device=sample_hidden_states.device)
 
         max_gen_len = sampled_token_ids.shape[-1]
         if max_gen_len == 1:
